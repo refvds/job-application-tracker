77fcec7527f81ab809b2a97cc51806dbae36f927
refvds <refvdsk@gmail.com>
2025-02-03 15:21:58 -0800

    [style-feat]: add styled status badges

--- frontend/src/components/shared/table-columns.tsx
+++ frontend/src/components/shared/table-columns.tsx
@@ -1,8 +1,14 @@
 import { ColumnDef } from '@tanstack/react-table';
 import { IRecord } from '../types/record';
 import { SalaryCell } from './salary-cell';
+import { Badge } from '../ui/badge';
+import { cn } from '@/lib/utils';
 
-export const tableColumns: ColumnDef<IRecord>[] = [
+type CustomColumnDef<TData> = ColumnDef<TData> & {
+  headerClassName?: string;
+};
+
+export const tableColumns: CustomColumnDef<IRecord>[] = [
   {
     accessorKey: 'company',
     header: 'Company',
@@ -31,6 +37,20 @@ export const tableColumns: ColumnDef<IRecord>[] = [
     accessorKey: 'status',
     header: 'Status',
     enableSorting: true,
+    headerClassName: 'w-[150px] max-md:w-[80px]',
+    cell: ({ cell }) => {
+      return (
+        <Badge
+          variant='outline'
+          className={cn(
+            'p-[6px] rounded-md border-0',
+            cell.row.original.status.type,
+          )}
+        >
+          {cell.row.original.status.value}
+        </Badge>
+      );
+    },
   },
   {
     accessorKey: 'note',
